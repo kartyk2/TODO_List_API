54bad3eb0c0dc70b4ac173693ee72621b02d75ff
kartyk2 <kartik.official.acc@gmail.com>
2023-03-02 10:02:58 +0530

    add comment

--- Exceptions.py
+++ Exceptions.py
@@ -3,3 +3,5 @@ from fastapi import HTTPException
 invalid_user = HTTPException(status_code=401, detail="Invalid User")
 invalid_token = HTTPException(status_code=401, detail="Invalid Token")
 expired_token = HTTPException(status_code=401, detail="Expired Token")
+
+#added a comment
